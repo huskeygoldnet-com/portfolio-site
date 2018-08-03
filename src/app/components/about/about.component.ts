b10ad5d64cfffa5e1b967cf5cb3ed6a08785d59d
Zach Bayoff <zrbayoff@gmail.com>
2018-08-03 16:51:38 -0400

    adding portfolio section

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -19,7 +19,7 @@ export class AboutComponent implements OnInit {
 
   canvasContainer: any;
 
-  @HostListener('window:resize', ['$event'])
+  // @HostListener('window:resize', ['$event'])
 
 
 
@@ -33,7 +33,7 @@ export class AboutComponent implements OnInit {
   onResize(event) {
 
 
-    (<HTMLElement>document.querySelector('.canvas-container canvas')).setAttribute('width', `${event.target.innerWidth}`);
+    // (<HTMLElement>document.querySelector('.canvas-container canvas')).setAttribute('width', `${event.target.innerWidth}`);
     // console.log(this.canvas.innerWidth);
     // console.log(event.target.innerWidth);
     // this.canvas = document.querySelector('.canvas-container').style.width = 50;
@@ -46,110 +46,110 @@ export class AboutComponent implements OnInit {
   // }
 
   constructor(private elementRef: ElementRef) {
-    this.elementRef = elementRef.nativeElement;
+    // this.elementRef = elementRef.nativeElement;
   }
 
-  runGravityBalls() {
-    this.canvasContainer = document.querySelector('.canvas-container');
-    // console.log(this.canvas.nativeElement.width);
-    // canvas.width = window.innerWidth;
-    // console.log(this.canvas);
-    const width = 800,
-      height = 800;
+  // runGravityBalls() {
+  //   this.canvasContainer = document.querySelector('.canvas-container');
+  //   // console.log(this.canvas.nativeElement.width);
+  //   // canvas.width = window.innerWidth;
+  //   // console.log(this.canvas);
+  //   const width = 800,
+  //     height = 800;
 
-    // canvas.style.width = width;
-    // canvas.height = height;
-    const Engine = Matter.Engine,
-      Render = Matter.Render,
-      Runner = Matter.Runner,
-      Composites = Matter.Composites,
-      Common = Matter.Common,
-      MouseConstraint = Matter.MouseConstraint,
-      Mouse = Matter.Mouse,
-      World = Matter.World,
-      Bodies = Matter.Bodies;
+  //   // canvas.style.width = width;
+  //   // canvas.height = height;
+  //   const Engine = Matter.Engine,
+  //     Render = Matter.Render,
+  //     Runner = Matter.Runner,
+  //     Composites = Matter.Composites,
+  //     Common = Matter.Common,
+  //     MouseConstraint = Matter.MouseConstraint,
+  //     Mouse = Matter.Mouse,
+  //     World = Matter.World,
+  //     Bodies = Matter.Bodies;
 
-    // create engine
-    const engine = Engine.create(),
-      world = engine.world;
+  //   // create engine
+  //   const engine = Engine.create(),
+  //     world = engine.world;
 
-    // create renderer
-    const render = Render.create({
-      element: this.canvasContainer,
-      engine: engine,
-      options: {
-        width: 800,
-        height: 600,
-        showAngleIndicator: true,
-      }
-    });
+  //   // create renderer
+  //   const render = Render.create({
+  //     element: this.canvasContainer,
+  //     engine: engine,
+  //     options: {
+  //       width: 800,
+  //       height: 600,
+  //       showAngleIndicator: true,
+  //     }
+  //   });
 
-    Render.run(render);
+  //   Render.run(render);
 
-    // create runner
-    const runner = Runner.create();
-    Runner.run(runner, engine);
+  //   // create runner
+  //   const runner = Runner.create();
+  //   Runner.run(runner, engine);
 
-    // add bodies
-    const stack = Composites.stack(20, 20, 10, 5, 0, 0, function (x, y) {
-      let sides = Math.round(Common.random(1, 8));
+  //   // add bodies
+  //   const stack = Composites.stack(20, 20, 10, 5, 0, 0, function (x, y) {
+  //     let sides = Math.round(Common.random(1, 8));
 
-      // triangles can be a little unstable, so avoid until fixed
-      sides = (sides === 3) ? 4 : sides;
+  //     // triangles can be a little unstable, so avoid until fixed
+  //     sides = (sides === 3) ? 4 : sides;
 
-      // round the edges of some bodies
-      let chamfer = null;
-      if (sides > 2 && Common.random() > 0.7) {
-        chamfer = {
-          radius: 10
-        };
-      }
+  //     // round the edges of some bodies
+  //     let chamfer = null;
+  //     if (sides > 2 && Common.random() > 0.7) {
+  //       chamfer = {
+  //         radius: 10
+  //       };
+  //     }
 
-      switch (Math.round(Common.random(0, 1))) {
-        case 0:
-          if (Common.random() < 0.8) {
-            return Bodies.rectangle(x, y, Common.random(25, 50), Common.random(25, 50), { chamfer: chamfer });
-          } else {
-            return Bodies.rectangle(x, y, Common.random(80, 120), Common.random(25, 30), { chamfer: chamfer });
-          }
-        case 1:
-          return Bodies.polygon(x, y, sides, Common.random(25, 50), { chamfer: chamfer });
-      }
-    });
+  //     switch (Math.round(Common.random(0, 1))) {
+  //       case 0:
+  //         if (Common.random() < 0.8) {
+  //           return Bodies.rectangle(x, y, Common.random(25, 50), Common.random(25, 50), { chamfer: chamfer });
+  //         } else {
+  //           return Bodies.rectangle(x, y, Common.random(80, 120), Common.random(25, 30), { chamfer: chamfer });
+  //         }
+  //       case 1:
+  //         return Bodies.polygon(x, y, sides, Common.random(25, 50), { chamfer: chamfer });
+  //     }
+  //   });
 
-    World.add(world, stack);
+  //   World.add(world, stack);
 
-    World.add(world, [
-      // walls
-      Bodies.rectangle(400, 0, 800, 50, { isStatic: true }),
-      Bodies.rectangle(400, 600, 800, 50, { isStatic: true }),
-      Bodies.rectangle(800, 300, 50, 600, { isStatic: true }),
-      Bodies.rectangle(0, 300, 50, 600, { isStatic: true })
-    ]);
+  //   World.add(world, [
+  //     // walls
+  //     Bodies.rectangle(400, 0, 800, 50, { isStatic: true }),
+  //     Bodies.rectangle(400, 600, 800, 50, { isStatic: true }),
+  //     Bodies.rectangle(800, 300, 50, 600, { isStatic: true }),
+  //     Bodies.rectangle(0, 300, 50, 600, { isStatic: true })
+  //   ]);
 
-    // add mouse control
-    const mouse = Mouse.create(render.canvas),
-      mouseConstraint = MouseConstraint.create(engine, {
-        mouse: mouse,
-        constraint: {
-          stiffness: 0.2,
-          render: {
-            visible: false
-          }
-        }
-      });
+  //   // add mouse control
+  //   const mouse = Mouse.create(render.canvas),
+  //     mouseConstraint = MouseConstraint.create(engine, {
+  //       mouse: mouse,
+  //       constraint: {
+  //         stiffness: 0.2,
+  //         render: {
+  //           visible: false
+  //         }
+  //       }
+  //     });
 
-    World.add(world, mouseConstraint);
+  //   World.add(world, mouseConstraint);
 
-    // keep the mouse in sync with rendering
-    render.mouse = mouse;
+  //   // keep the mouse in sync with rendering
+  //   render.mouse = mouse;
 
-    // fit the render viewport to the scene
-    Render.lookAt(render, {
-      min: { x: 0, y: 0 },
-      max: { x: 800, y: 600 }
-    });
-  }
+  //   // fit the render viewport to the scene
+  //   Render.lookAt(render, {
+  //     min: { x: 0, y: 0 },
+  //     max: { x: 800, y: 600 }
+  //   });
+  // }
 
   ngOnInit() {
 
--- src/app/data/portfolio.model.ts
+++ src/app/data/portfolio.model.ts
@@ -2,13 +2,15 @@ export const portfolio_list = [
     {
         name: 'Travel App',
         route_url: 'travelapp',
+        short_desc: 'An app to organize your travel destinations and costs.',
         description: 'This web application was created for those who like to travel, but may not have an easy way to keep track of all of their desired destinations and associated expenses. With Travel App (pardon the lack of inticing app name), a user can create, update, and delete a "trip", filling in details such as the trip destination, travel dates, and list of costs one might incur while traveling. This app also calls the Unsplash API that allows the user to find high resolution, beautiful photographs of just about anything, and could maybe help remind them of the beauties that their destinations hold.A MongoDB database was set up to store the users trips, meaning that anyone can access anyone\'s trips. This was implemented more on a proof-of-concept basis than an actual user authentication app. I may add this implementation in the future.',
-        img: '../assets/images/travel-app-screenshot.png'
+        img: '../assets/images/project-desc-travelapp.png'
     },
     {
         name: 'Weather App',
         route_url: 'weatherapp',
+        short_desc: 'Get current weather data for any city/country in the world.',
         description: 'This application was my first exposure to building an application using the AngularJS framework. A simple, single page application that makes call to the OpenWeatherMap API via a city/zip code and country combination. The weather data is retrieved via JSON and displayed with a high resolution background image to indicate the type of weather at the current time and place. The user can switch between Farenheit and Celcius. A Google Map is also embedded to indicate the latitude and longitude of the location the user has requested.',
-        img: '../assets/images/weather-app-screenshot.png'
+        img: '../assets/images/project-desc-weatherapp.png'
     }
 ];
