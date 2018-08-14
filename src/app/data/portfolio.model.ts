09e6dff6f0789e9c9d49a7449227f6cfc729ef0c
Zach Bayoff <zrbayoff@gmail.com>
2018-08-14 17:20:54 -0400

    updating portfolio desc page

--- src/app/data/portfolio.model.ts
+++ src/app/data/portfolio.model.ts
@@ -7,8 +7,19 @@ export const portfolio_list = [
         completed_date: 'July 2018',
         description: 'I developed a WordPress site for a a nascent startup that was in need of a landing page and eCommerce functionality in order to sell eTickets for their platform. I decided to use WordPress because it allowed me to deliver the site to the client quickly with a professional feel and eCommerce capabilities built in. \n\nThe site includes the WooCommerce plugin as well as a custom plugin and child-theme that I developed to allow me to write custom CSS and Javascript.',
         technologies: ['WordPress', 'PHP', 'WooCommerce', 'Stripe', 'jQuery'],
-        img: '../assets/images/project-desc-circlebreak.png',
-        screenshots: ['']
+        desc_img: '../assets/images/portfolio/circlebreak/project-desc-circlebreak.png',
+        home_screenshots:
+        {
+            laptop: '../assets/images/portfolio/circlebreak/home-screenshot-laptop.png',
+            ipad: '../assets/images/portfolio/circlebreak/home-screenshot-ipad.png',
+            iphone: '../assets/images/portfolio/circlebreak/home-screenshot-iphone.png'
+        },
+        other_screenshots: [
+            '../assets/images/portfolio/circlebreak/home-fullpage.png',
+            '../assets/images/portfolio/circlebreak/ticketstore-page.png',
+            '../assets/images/portfolio/circlebreak/checkout-page.png',
+            '../assets/images/portfolio/circlebreak/contact-us-page.png',
+        ]
     },
     {
         name: 'Roomies',
@@ -18,9 +29,17 @@ export const portfolio_list = [
         completed_date: 'March 2018',
         description: 'Roomies is an application that lets roommates easily manage their chores, schedule visitors, and keep track of household purchases. Roommates are part of a "group" and may join several groups. Roomates can add their phone numbers to be automatically notified about chore dates, visitor arrivals, and household item\'s bills.',
         technologies: ['PHP', 'MySQL', 'jQuery', 'Bootstrap4', 'AJAX'],
-        img: '../assets/images/project-desc-roomies.png',
-        screenshots: ['../assets/images/portfolio/roomies/login-page.png', '../assets/images/portfolio/roomies/signup-page.png',
-        '../assets/images/portfolio/roomies/groups-page.png', '../assets/images/portfolio/roomies/chores-page.png', '../assets/images/portfolio/roomies/items-page.png', '../assets/images/portfolio/roomies/visitors-page.png', '../assets/images/portfolio/roomies/preferences-page.png']
+        desc_img: '../assets/images/portfolio/roomies/project-desc-roomies.png',
+        home_screenshots:
+        {
+            laptop: '../assets/images/portfolio/roomies/signup-page.png',
+            ipad: '../assets/images/portfolio/roomies/signup-page.png',
+            iphone: '../assets/images/portfolio/roomies/signup-page.png'
+        },
+        other_screenshots: [
+            '../assets/images/portfolio/roomies/login-page.png', '../assets/images/portfolio/roomies/signup-page.png',
+            '../assets/images/portfolio/roomies/groups-page.png', '../assets/images/portfolio/roomies/chores-page.png', '../assets/images/portfolio/roomies/items-page.png', '../assets/images/portfolio/roomies/visitors-page.png', '../assets/images/portfolio/roomies/preferences-page.png'
+        ]
     },
     {
         name: 'Travel App',
@@ -30,7 +49,18 @@ export const portfolio_list = [
         completed_date: 'May 2018',
         description: 'This web application was created for those who like to travel, but may not have an easy way to keep track of all of their desired destinations and associated expenses. With Travel App (pardon the lack of inticing app name), a user can create, update, and delete a "trip", filling in details such as the trip destination, travel dates, and list of costs one might incur while traveling. \n\nThis app also calls the Unsplash API that allows the user to find high resolution, beautiful photographs of just about anything, and could maybe help remind them of the beauties that their destinations hold. A MongoDB database was set up to store the users trips, meaning that anyone can access anyone\'s trips. This was implemented more on a proof-of-concept basis than an actual user authentication app. I may add this implementation in the future.',
         technologies: ['Angular 6', 'SASS', 'NodeJS', 'Express', 'MongoDB'],
-        img: '../assets/images/project-desc-travelapp.png'
+        desc_img: '../assets/images/portfolio/travelapp/project-desc-travelapp.png',
+        home_screenshots:
+        {
+            laptop: '../assets/images/portfolio/travelapp/home-screenshot-laptop.png',
+            ipad: '../assets/images/portfolio/travelapp/home-screenshot-ipad.png',
+            iphone: '../assets/images/portfolio/travelapp/home-screenshot-iphone.png'
+        },
+        other_screenshots: [
+            '../assets/images/portfolio/travelapp/travel-app-screenshot.png',
+            '../assets/images/portfolio/travelapp/travel-app-screenshot2.png',
+            '../assets/images/portfolio/travelapp/travel-app-screenshot3.png'
+        ]
     },
     {
         name: 'Weather App',
@@ -38,19 +68,37 @@ export const portfolio_list = [
         abs_url: 'https://zb-weather-app.herokuapp.com',
         short_desc: 'Get current weather data for any city/country in the world.',
         completed_date: 'May 2018',
-        description: 'This application was my first exposure to building an application using the AngularJS framework. A simple, single page application that makes call to the OpenWeatherMap API via a city/zip code and country combination. The weather data is retrieved via JSON and displayed with a high resolution background image to indicate the type of weather at the current time and place. The user can switch between Farenheit and Celcius. A Google Map is also embedded to indicate the latitude and longitude of the location the user has requested.',
+        description: 'Weather App is a simple, single page application that makes a call to the OpenWeatherMap API via a city/zip code and country combination. The weather data is retrieved via JSON and displayed with a high resolution background image to indicate the type of weather at the current time and place. The user can switch between Farenheit and Celcius. A Google Map is also embedded to indicate the latitude and longitude of the location the user has requested.',
         technologies: ['AngularJS', 'Webpack', 'Express', 'SASS'],
-        img: '../assets/images/project-desc-weatherapp.png'
+        desc_img: '../assets/images/portfolio/weatherapp/project-desc-weatherapp.png',
+        home_screenshots:
+        {
+            laptop: '../assets/images/portfolio/weatherapp/home-screenshot-laptop.png',
+            ipad: '../assets/images/portfolio/weatherapp/home-screenshot-ipad.png',
+            iphone: '../assets/images/portfolio/weatherapp/home-screenshot-iphone.png'
+        },
+        other_screenshots: [
+            '../assets/images/portfolio/weatherapp/full-page.png',
+        ]
     },
     {
-        name: 'Bayground',
+        name: 'The Bayground',
         route_url: 'bayground',
         abs_url: 'https://zbayoff.github.io/bayground/',
-        short_desc: 'A set of small games and apps created to enhance my ES6 skills.',
+        short_desc: 'Mini javascript games and apps.',
         completed_date: 'January 2018',
-        description: 'The Bayground is a collection of small, simple to complex applications. They range from finding a set of prime numbers to playing a game of concentration.This project was created to enhance javascript and programming skills. Namely, creating and using functions in different ways, writing loops, manipulating the DOM (without jQuery), adhering to ECMAScript 6 specifications, and much more.',
+        description: 'The Bayground is a collection of small, simple to complex applications. They range from finding a set of prime numbers to playing a game of concentration. This project was created to enhance javascript and programming skills. Namely, creating and using functions in different ways, writing loops, manipulating the DOM (without jQuery), adhering to ECMAScript 6 specifications, and much more.',
         technologies: ['Vanilla Javascript'],
-        img: '../assets/images/project-desc-bayground.png'
+        desc_img: '../assets/images/portfolio/bayground/project-desc-bayground.png',
+        home_screenshots:
+        {
+            laptop: '../assets/images/portfolio/bayground/project-desc-bayground.png',
+            ipad: '../assets/images/portfolio/bayground/project-desc-bayground.png',
+            iphone: '../assets/images/portfolio/bayground/project-desc-bayground.png'
+        },
+        other_screenshots: [
+            '../assets/images/portfolio/bayground/project-desc-bayground.png'
+        ]
     },
     {
         name: 'New York Junior Tennis League',
@@ -60,6 +108,16 @@ export const portfolio_list = [
         completed_date: 'November 2017',
         description: 'I chose to build this project out of my love for tennis (it was also a midterm project for my CUNY City Tech Course). This project was built by using content from NYJTL and creating a new look and feel. Using Bootstrap4, the site includes many modern features that makes Bootstrap so easy to use as a front-end framework: Carousels, modals, accordians, hamburger menu, etc. Many of the links do not work, as this is more of a concept site. The donation page is set up, though, and functions like a typical donation system would work.',
         technologies: ['Bootstrap4', 'Javascript'],
-        img: '../assets/images/project-desc-nyjtl.png'
+        desc_img: '../assets/images/portfolio/nyjtl/project-desc-nyjtl.png',
+        home_screenshots:
+        {
+            laptop: '../assets/images/portfolio/nyjtl/home-screenshot-laptop.png',
+            ipad: '../assets/images/portfolio/nyjtl/home-screenshot-ipad.png',
+            iphone: '../assets/images/portfolio/nyjtl/home-screenshot-iphone.png'
+        },
+        other_screenshots: [
+            '../assets/images/portfolio/nyjtl/home-fullpage.png',
+            '../assets/images/portfolio/nyjtl/donations-screenshot.png'
+        ]
     }
 ];
